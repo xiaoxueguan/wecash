7f53b522b3e05fb2b833cf7c70df702edbb8f3ce
xiaoxueguan <zhangke@wecash.net>
2014-10-28 18:08:15 +0800

    bash.sh

---
+++ bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget http://mirrors.aliyun.com/fix_stuff/bash_4.2-2ubuntu2.2_amd64.deb && dpkg -i  bash_4.2-2ubuntu2.2_amd64.deb
+
+dpkg -i bash_4.2-2ubuntu2.2_amd64.deb
+
+rm -f bash_4.2-2ubuntu2.2_amd64.deb
+
+rm -f bash.sh
+
+env x='() { :;}; echo vulnerable' bash -c "echo this is a test"
